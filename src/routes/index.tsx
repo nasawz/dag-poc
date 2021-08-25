7fc5dba56c46d95b0743d6157042148c8cd5ab4e
lijie <lijie@DESKTOP-BNH9NOI.localdomain>
2021-08-25 10:40:53 +0800

    add route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,50 +1,15 @@
-import { Layout } from "antd";
 import * as React from "react";
-import { GuideHeader } from "../layout/header";
-import styles from "../style/index.module.less";
-import classNames from "classnames";
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
-import { ComponentTreePanel } from "../components/component-tree-panel";
-import { ComponentConfigPanel } from "../components/component-config-panel";
-import { DAGCanvas } from "../components/dag-canvas";
 
-import { socket } from '../web-sockets'
-
-export interface IAppProps {
-  experimentId?;
-}
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import TemplateComponent from "./template.component"
+export interface IAppProps { }
 
 export default function App(props: IAppProps) {
-  const { experimentId = "2" } = props;
-  const { Content } = Layout;
-  socket.on('welcome', (data) => {
-    console.log("Welcome event inside JoinRoom", data);
-    // props.onJoinSuccess(data);
-  });
   return (
-    <Layout className={styles.layout}>
-      <GuideHeader experimentId={experimentId} />
-      <Content className={styles.content}>
-        <div className={classNames(styles.experiment)}>
-          <DndProvider backend={HTML5Backend}>
-            <ComponentTreePanel
-              experimentId={experimentId}
-              className={styles.nodeSourceTree}
-            />
-            <div className={styles.editPanel}>
-              <DAGCanvas
-                experimentId={experimentId}
-                className={styles.dagCanvas}
-              />
-              <ComponentConfigPanel
-                experimentId={experimentId}
-                className={styles.confPanel}
-              />
-            </div>
-          </DndProvider>
-        </div>
-      </Content>
-    </Layout>
+    <Router basename={"/experiment"}>
+      <Switch>
+        <Route path={"/:experimentId"} component={TemplateComponent} />
+      </Switch>
+    </Router>
   );
 }
---
+++ src/routes/template.component.tsx
@@ -0,0 +1,49 @@
+import { Layout } from "antd";
+import * as React from "react";
+import { GuideHeader } from "../layout/header";
+import styles from "../style/index.module.less";
+import classNames from "classnames";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import { ComponentTreePanel } from "../components/component-tree-panel";
+import { ComponentConfigPanel } from "../components/component-config-panel";
+import { DAGCanvas } from "../components/dag-canvas";
+import { socket } from '../web-sockets'
+import { useParams } from "react-router-dom";
+export interface ITemplateComponentProps {
+    experimentId?;
+}
+
+export default function TemplateComponent(props: ITemplateComponentProps) {
+    const { experimentId } = useParams()
+    const { Content } = Layout;
+    socket.on('welcome', (data) => {
+        console.log("Welcome event inside JoinRoom", data);
+        // props.onJoinSuccess(data);
+    });
+    return (
+        <Layout className={styles.layout}>
+            <GuideHeader experimentId={experimentId} />
+            <Content className={styles.content}>
+                <div className={classNames(styles.experiment)}>
+                    <DndProvider backend={HTML5Backend}>
+                        <ComponentTreePanel
+                            experimentId={experimentId}
+                            className={styles.nodeSourceTree}
+                        />
+                        <div className={styles.editPanel}>
+                            <DAGCanvas
+                                experimentId={experimentId}
+                                className={styles.dagCanvas}
+                            />
+                            <ComponentConfigPanel
+                                experimentId={experimentId}
+                                className={styles.confPanel}
+                            />
+                        </div>
+                    </DndProvider>
+                </div>
+            </Content>
+        </Layout>
+    );
+}
