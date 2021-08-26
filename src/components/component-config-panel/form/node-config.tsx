0db9ce27c7d27b3cb9a0212a1dae101f20eb05b6
danwbj <danwbj@163.com>
2021-08-26 15:00:57 +0800

    增加系数配置

--- src/components/component-config-panel/form/node-config.tsx
+++ src/components/component-config-panel/form/node-config.tsx
@@ -1,10 +1,12 @@
 import React from "react";
-import { Divider, Form, Input } from "antd";
+import { Divider, Form, Input, InputNumber, Mentions, Row, Tag } from "antd";
 import { keys, map } from "lodash-es";
 // import { useExperimentGraph } from '@/pages/rx-models/experiment-graph'
 import "antd/lib/style/index.css";
 import { useObservableState } from "../../../hooks/useObservableState";
 import { useExperimentGraph } from "../../../rx-models/experiment-graph";
+import { useState } from "react";
+import { useEffect } from "react";
 
 export interface Props {
   name: string;
@@ -19,11 +21,44 @@ export const NodeFormDemo: React.FC<Props> = ({
 }) => {
   const [form] = Form.useForm();
 
+  const [displayExpression, setDisplayExpression] = useState();
   const expGraph = useExperimentGraph(experimentId);
   const [node] = useObservableState(() => expGraph.activeNodeInstance$);
-  console.log("[activeNodeInstance]", node);
+
+  // 展示计算公式
+  const renderExpression = (expression) => {
+    if (!expression) {
+      return "-";
+    }
+    const expressionTitle = expression;
+    if (expression) {
+      const expressionArr = expression.split(" ");
+      return expressionArr.map((item, index) => {
+        const patt = /([/#]{1})([\p{Unified_Ideograph}|\w(\/\w)?]+)/gu;
+        if (patt.test(item)) {
+          return (
+            <Tag key={index} color="blue" style={{ marginBottom: 8 }}>
+              {item}
+            </Tag>
+          );
+        } else {
+          return (
+            <span key={index} style={{ marginRight: 8, marginBottom: 8 }}>
+              {item}
+            </span>
+          );
+        }
+      });
+    }
+    return expressionTitle;
+  };
 
   const onValuesChange = async ({ name, ...others }: { name: string }) => {
+    //计算展示的expression
+    if (others["expression"] && node.codeName === "source_sum") {
+      replaceNodeIdToName(others["expression"]);
+    }
+
     if (name && node.name !== name) {
       await expGraph.renameNode(nodeId, name);
     }
@@ -50,9 +85,21 @@ export const NodeFormDemo: React.FC<Props> = ({
     return <div />;
   }
 
-  // console.log("nodeId============================>>>", nodeId);
   let sourceNodes = expGraph.findSourceByNode(nodeId);
-  console.log(sourceNodes, "=======");
+
+  const replaceNodeIdToName = (expression) => {
+    let newStr = expression;
+    sourceNodes.forEach((item) => {
+      newStr = newStr.replaceAll(item.id, item.name);
+    });
+    setDisplayExpression(newStr);
+  };
+
+  useEffect(() => {
+    if (node && node.codeName === "source_sum") {
+      replaceNodeIdToName(node?.data?.expression);
+    }
+  }, [node]);
 
   return (
     <Form
@@ -69,13 +116,29 @@ export const NodeFormDemo: React.FC<Props> = ({
       <Form.Item label="表.字段" name="table_field">
         <Input placeholder="input placeholder" />
       </Form.Item>
-      {/* <Form.Item label={name}>
-        <Input placeholder="input placeholder" />
-      </Form.Item>
-      
-      <Form.Item label="Field D">
-        <Input placeholder="input placeholder" />
-      </Form.Item> */}
+
+      {/* 公式 */}
+      {node.codeName === "source_sum" && (
+        <div>
+          <Form.Item name="expression" label="计算公式">
+            <Mentions rows={3} prefix={["#"]}>
+              {sourceNodes.map(({ id, name }) => (
+                <Mentions.Option value={`s_${id}`} key={id}>
+                  {name}
+                </Mentions.Option>
+              ))}
+            </Mentions>
+          </Form.Item>
+          <div>{renderExpression(displayExpression)}</div>
+        </div>
+      )}
+
+      {/* 系数 */}
+      {node.codeName === "constant" && (
+        <Form.Item name="constant" label="系数">
+          <InputNumber min={0} style={{ width: "100%" }} />
+        </Form.Item>
+      )}
     </Form>
   );
 };
